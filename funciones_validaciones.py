59dca51a228ad4346f05dced64a59602a3dd5437
romina85castro-maker <romina85castro@gmail.com>
2026-06-01 19:19:21 -0300

    Agrego validacion del menu en main  y completo lo que faltaba del menu menu

--- funciones_validaciones.py
+++ funciones_validaciones.py
@@ -6,3 +6,24 @@ def validar_campo_vacio(texto):
     if texto != "":
         return True
     return False
+
+import questionary
+
+def menu():
+    print("""
+=================================
+            M E N Ú 
+=================================""")
+    return questionary.select(
+        "Seleccione una opción:",
+        choices=[
+            "1 - Agregar Pais",
+            "2 - Actualizar Pais",
+            "3 - Buscar Pais",
+            "4 - Filtrar Paises",
+            "5 - Ordenar Paises",
+            "6 - Mostrar Estadisticas",
+            "7 - Salir"
+
+        ]
+    ).ask()
--- main.py
+++ main.py
@@ -1,17 +1,28 @@
-import questionary
+import funciones_validaciones  as fv
 
-def menu():
-    print("""
-=================================
-            M E N Ú 
-=================================""")
-    return questionary.select(
-        "Seleccione una opción:",
-        choices=[
-            "1 - Agregar",
-            "2 - Ver"
+opcion = fv.menu()
 
-        ]
-    ).ask()
+print(opcion)
 
-menu()
+if opcion == "1 - Agregar Pais":
+    print("Entro en Opcion 1")
+            
+elif opcion == "2 - Actualizar Pais":
+    print("Entro en Opcion 2")
+
+elif opcion == "3 - Buscar Pais":
+    print("Entro en Opcion 3")
+
+elif opcion == "4 - Filtrar Paises":
+    print("Entro en Opcion 4")
+
+elif opcion == "5 - Ordenar Paises":
+    print("Entro en Opcion 5")
+
+elif opcion == "6 - Mostrar Estadisticas":
+    print("Entro en Opcion 6")
+
+elif opcion == "7 - Salir":
+    print("Entro en Opcion 7")
+
+    

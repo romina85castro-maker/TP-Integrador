881c6f41468dc32836bd5505155ba93a8a5080c3
romina85castro-maker <romina85castro@gmail.com>
2026-06-02 11:10:57 -0300

    backup
    hice los backup de ambos archivos para tenerlo de copia

---
+++ backup_main.py
@@ -0,0 +1,26 @@
+import funciones_validaciones  as fv
+
+opcion = fv.menu()
+
+print(opcion)
+
+if opcion == "1 - Agregar Pais":
+    print("Entro en Opcion 1")
+            
+elif opcion == "2 - Actualizar Pais":
+    print("Entro en Opcion 2")
+
+elif opcion == "3 - Buscar Pais":
+    print("Entro en Opcion 3")
+
+elif opcion == "4 - Filtrar Paises":
+    print("Entro en Opcion 4")
+
+elif opcion == "5 - Ordenar Paises":
+    print("Entro en Opcion 5")
+
+elif opcion == "6 - Mostrar Estadisticas":
+    print("Entro en Opcion 6")
+
+elif opcion == "7 - Salir":
+    print("Entro en Opcion 7")
